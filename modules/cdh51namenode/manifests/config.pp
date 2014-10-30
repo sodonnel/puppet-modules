09f365de4c220f8340c6f206a7b25b3bb80e3d51
Stephen ODonnell <stephen.odonnell@gmail.com>
2014-10-30 21:01:22 +0000

    Added some config for namenode

---
+++ modules/cdh51namenode/manifests/config.pp
@@ -0,0 +1,22 @@
+class cdh51namenode::config {
+
+  host { $::cdh51namenode::hostname:
+    ip => '192.168.57.6',
+  }
+  
+#  file { "/etc/hadoop/conf/hdfs-site.xml":
+#    ensure => present,
+#    content => template("cdh51namenode/hdfs-site.xml.erb"),
+#    owner => "root",
+#    group => "root",
+#  }
+
+#  file { "/etc/hadoop/conf/core-site.xml":
+#    ensure => present,
+#    content => template("cdh51namenode/core-site.xml.erb"),
+#    owner => "root",
+#    group => "root",
+#  }
+
+  
+}
--- modules/cdh51namenode/manifests/init.pp
+++ modules/cdh51namenode/manifests/init.pp
@@ -1,7 +1,8 @@
-class cdh51namenode {
+class cdh51namenode($hostname = namenode) {
   
   require cdh51java
   require cdh51repo
   
   contain cdh51namenode::install
+  contain cdh51namenode::config
 }
