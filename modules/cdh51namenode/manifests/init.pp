e0ffda85d6195b7209a0ced5d0b93d64680727d4
Stephen ODonnell <stephen.odonnell@gmail.com>
2014-11-03 23:11:19 +0000

    Fixed ordering of config, format, service etc

--- modules/cdh51namenode/manifests/init.pp
+++ modules/cdh51namenode/manifests/init.pp
@@ -8,5 +8,10 @@ class cdh51namenode($hostname = namenode) {
   contain cdh51namenode::format
   contain cdh51namenode::service
   contain cdh51namenode::tmpdir
+
+  # On first install, the changes to the config files cause the service
+  # to be started before the format setup is called. This forces
+  # format to be called before the service can be started.
+  Exec['format-namenode'] -> Service['hadoop-hdfs-namenode'] -> Class['cdh51namenode::tmpdir']
   
 }
