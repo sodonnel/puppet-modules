0e0dbdcaa8d115d76b9c72266b34031dfafbdce2
Stephen ODonnell <stephen.odonnell@gmail.com>
2015-09-17 12:26:40 +0100

    Make java rpm come from a local file in a shared folder and not a yum repo

--- modules/cdh51java/manifests/init.pp
+++ modules/cdh51java/manifests/init.pp
@@ -2,8 +2,10 @@ class cdh51java {
 
   # This package is a custom package and need to be deployed
   # on localyumrepo
-  package { [javajdk]:
+  package { 'javajdk':
     ensure => present,
+    provider => 'rpm',
+    source => 'file:///puppet_modules/rpms/javajdk-1.7.0-79.x86_64.rpm'
   }
 
 }
