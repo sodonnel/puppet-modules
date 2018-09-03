4f1330124c0c71d21d05fd3e543b9d2686da2a8e
Stephen ODonnell <stephen.odonnell@gmail.com>
2018-09-03 21:08:43 +0100

    Tidy up changes missed from earlier updates

--- modules/cdh/manifests/config/kerberos.pp
+++ modules/cdh/manifests/config/kerberos.pp
@@ -3,23 +3,25 @@ class cdh::config::kerberos {
   $namenodehostname = $::cdh::config::namenodehostname
   $secure           = $::cdh::config::secure
 
-  file { "/root/create_principals.sh":
-    ensure  => present,
-    content => template("cdh/create_principals.sh.erb"),
-    owner   => "root",
-    group   => "root",
-    mode    => "700",
-  }
+  if ($secure) {
+    file { "/root/create_principals.sh":
+      ensure  => present,
+      content => template("cdh/create_principals.sh.erb"),
+      owner   => "root",
+      group   => "root",
+      mode    => "700",
+    }
 
-  ->
+    ->
 
-  exec {'generate-principals':
-    path      => ['/usr/bin', '/bin', '/usr/local/bin' ],
-    user      => 'root',
-    command   => '/root/create_principals.sh',
-    logoutput => on_failure,
-    creates   => "/root/keytabs_generated",
-    timeout   => 0,
+    exec {'generate-principals':
+      path      => ['/usr/bin', '/bin', '/usr/local/bin' ],
+      user      => 'root',
+      command   => '/root/create_principals.sh',
+      logoutput => on_failure,
+      creates   => "/root/keytabs_generated",
+      timeout   => 0,
+    }
   }
 
 }
